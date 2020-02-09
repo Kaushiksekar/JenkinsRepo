6ea614256620311e9a18f57ebf0c03a890c453d8
Kaushiksekar <kaushik3993@gmail.com>
2020-02-09 18:41:07 +0530

    Commit for load

--- initial_trial/Main.groovy
+++ initial_trial/Main.groovy
@@ -2,7 +2,7 @@ import Stage;
 
 class Main{
 	static void main(String[] args) {
-		Stage s = new Stage();
+		Stage s = load 'Stage.groovy';
 		s.setName("Stage1");
 		println(s.getName());
 		// println("Hello World");
