3d91bec620516aeb39e6976dde11aa0213562db2
Kaushiksekar <kaushik3993@gmail.com>
2020-02-09 18:43:00 +0530

    Commit for load in JenkinsFile

--- initial_trial/Main.groovy
+++ initial_trial/Main.groovy
@@ -2,7 +2,7 @@ import Stage;
 
 class Main{
 	static void main(String[] args) {
-		Stage s = load 'Stage.groovy';
+		Stage s = new Stage();
 		s.setName("Stage1");
 		println(s.getName());
 		// println("Hello World");
